beca25553dc8a2acd195f371b44886dc74af0eef
oriolesinski-pixel <oriolesinski@gmail.com>
2025-10-06 22:46:58 +0300

    Create provider

---
+++ test-app-rich/app/components/AnalyticsProvider.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import React, { createContext, useState, useEffect } from 'react';
+
+export const AnalyticsContext = createContext({
+  appKey: '',
+  sessionId: '',
+  userId: null as string | null
+});
+
+function AnalyticsProvider({ 
+  children, 
+  userId = null 
+}: { 
+  children: React.ReactNode;
+  userId?: string | null;
+}) {
+  const [sessionId, setSessionId] = useState('');
+  
+  useEffect(() => {
+    try {
+      let sid = sessionStorage.getItem('analytics_session_id');
+      if (!sid) {
+        sid = 'sess_' + Date.now() + '_' + Math.random().toString(36).substr(2, 9);
+        sessionStorage.setItem('analytics_session_id', sid);
+      }
+      setSessionId(sid);
+    } catch {
+      setSessionId('sess_' + Date.now() + '_' + Math.random().toString(36).substr(2, 9));
+    }
+  }, []);
+  
+  return (
+    <AnalyticsContext.Provider value={{
+      appKey: 'demo-test-apps-2025-10-06-yz9g7xd46fr',
+      sessionId,
+      userId
+    }}>
+      {children}
+    </AnalyticsContext.Provider>
+  );
+}
+
+export default AnalyticsProvider;
